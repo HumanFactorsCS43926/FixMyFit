e6b6fe63dd7fbf8b63012d0f8a99cacdd2a4301b
Yuvanesh Rajamani <jjd149@txstate.edu>
2023-04-09 17:30:31 -0500

    Auto stash before merge of "fixPosting" and "origin/fixPosting"

--- FixMyFit-app-main/src/page/Posts.js
+++ FixMyFit-app-main/src/page/Posts.js
@@ -10,8 +10,16 @@ const Posts = () => {
         const collectionRef = collection(db,"post")
         const q = query(collectionRef,orderBy("timestamp","desc"))
         const unsubscribe = onSnapshot(q, (querySnapshot)=>{
-            setPosts(querySnapshot.docs.map(doc => ({...doc.data(),id: doc.id, timestamp: doc.data().timestamp?.toDate().getTime() })))
-        })
+          setPosts(querySnapshot.docs.map(doc => {
+              console.log(doc.data()); // add this line to log the data object
+              return {
+                  ...doc.data(),
+                  id: doc.id,
+                  timestamp: doc.data().timestamp?.toDate().getTime(),
+                  username: doc.data().userName?.userName
+              }
+          }));
+      });
 
         return unsubscribe
     },[])
@@ -23,13 +31,13 @@ const Posts = () => {
           {posts.map((post) => (
             <div key={post.id} className='bg-white rounded-lg shadow-xl p-8 w-1/2 m-auto mb-4'>
 
-              <div className='text-lg'>{post.post}</div>
+              <div className='text-lg'>{post.userName?.userName} - {post.post}</div>
               <div className='flex space-x-3'>
                 {post.images.map((image, index) => (
                   <img key={index}  src={image} width={"200px"} />
                 ))}
               </div>
-              <p className='mt-3 text-xs text-right text-gray-400'>{moment(post.timnestamp).fromNow()}</p>
+              <p className='mt-3 text-xs text-right text-gray-400'>{moment(post.timestamp).fromNow()}</p>
             </div>
           ))}
         </div>
--- FixMyFit-app-main/src/page/dropzone.js
+++ FixMyFit-app-main/src/page/dropzone.js
@@ -1,64 +1,105 @@
-import React, { useCallback ,useState, useRef} from 'react'
+import React, { useCallback, useState, useRef, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
-import {db,storage,useAuth} from '../firebase';
-import { addDoc, arrayUnion,updateDoc,doc, collection, serverTimestamp } from 'firebase/firestore';
-import { ref, getDownloadURL, uploadBytes} from "@firebase/storage";
+import { db, storage, useAuth } from '../firebase';
+import {
+  addDoc,
+  getDoc,
+  arrayUnion,
+  updateDoc,
+  doc,
+  collection,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { ref, getDownloadURL, uploadBytes } from '@firebase/storage';
 
 export default function Dropzone() {
   const currentUser = useAuth();
-  const [selectedImages, setSelectedImages]= useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [userData, setUserData] = useState(null); // state to store user data
   const postRef = useRef(null);
 
-  const uploadPost = async()=>{
-    const docRef = await addDoc(collection(db,"post"),{
-      post:postRef.current.value,
-      timestamp:serverTimestamp(),
-      user:currentUser.uid,
-      images: selectedImages.map(image => image.downloadURL)
-    })
-    await Promise.all(
-      selectedImages.map(image=>{
-        const imageRef = ref(storage, `post/${docRef.id}/${image.path}`);
-        uploadBytes(imageRef,image,"data_url").then(async()=>{
-          const downloadURL = await getDownloadURL(imageRef)
-          await updateDoc(doc(db,"post",docRef.id),{
-            images:arrayUnion(downloadURL)
-          })
-        })
-      })
-    )
-    postRef.current.value='';
-    setSelectedImages([]);
+  const getUserData = async () => {
+    if (currentUser) {
+      const userDoc = await getDoc(doc(db, 'users', currentUser.uid));
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        console.log(userData.userName);
+        setUserData(userData);
+      } else {
+        console.log("User not found");
+      }
+    } else {
+      console.log("No user is currently logged in");
+    }
   }
 
-  const onDrop = useCallback(acceptedFiles =>{
-    setSelectedImages(acceptedFiles.map(file=>
-        Object.assign(file,{
-            preview:URL.createObjectURL(file),
-            downloadURL: ''
-        })))
+  const uploadPost = async () => {
+    if (!userData) {
+      console.log("User data is not available yet");
+      return;
+    }
 
-  },[])
+    const docRef = await addDoc(collection(db, 'post'), {
+      post: postRef.current.value,
+      timestamp: serverTimestamp(),
+      user: currentUser.uid,
+      userName: userData.userName,
+      images: selectedImages.map((image) => image.downloadURL),
+    });
+    await Promise.all(
+      selectedImages.map((image) => {
+        const imageRef = ref(
+          storage,
+          `post/${docRef.id}/${image.path}`
+        );
+        uploadBytes(imageRef, image, 'data_url').then(async () => {
+          const downloadURL = await getDownloadURL(imageRef);
+          await updateDoc(doc(db, 'post', docRef.id), {
+            images: arrayUnion(downloadURL),
+          });
+        });
+      })
+    );
+    postRef.current.value = '';
+    setSelectedImages([]);
+  };
 
-  const {getRootProps,getInputProps} = useDropzone({onDrop})
+  const onDrop = useCallback((acceptedFiles) => {
+    setSelectedImages(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+          downloadURL: '',
+        })
+      )
+    );
+  }, []);
 
-  const selected_images = selectedImages?.map(file=>(
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  const selected_images = selectedImages?.map((file) => (
     <div>
-        <img src={file.preview} style={{width:"200px"}}alt=""/>
+      <img src={file.preview} style={{ width: '200px' }} alt="" />
     </div>
-  ))
+  ));
 
   const isUploadDisabled = selectedImages.length === 0;
 
-  return(
+  useEffect(() => {
+    getUserData();
+  }, [currentUser]);
+
+  return (
     <div>
-      <input ref={postRef} type="text" placeholder='enter a post'/>
+      <input ref={postRef} type="text" placeholder="enter a post" />
       <div {...getRootProps()}>
-          <input {...getInputProps()}/>
-          <p>Drop the the files here</p>
+        <input {...getInputProps()} />
+        <p>Drop the the files here</p>
       </div>
-      <button onClick={uploadPost} disabled={isUploadDisabled}>post</button>
+      <button onClick={uploadPost} disabled={isUploadDisabled}>
+      </button>
       {selected_images}
     </div>
-  )
+  );
 }
+
