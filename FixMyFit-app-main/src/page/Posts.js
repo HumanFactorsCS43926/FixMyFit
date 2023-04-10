8f1326e994a01cd0a691fa867241eae0522d6135
Yuvanesh Rajamani <jjd149@txstate.edu>
2023-04-09 21:06:31 -0500

    got comments to work

--- FixMyFit-app-main/src/page/Posts.js
+++ FixMyFit-app-main/src/page/Posts.js
@@ -1,64 +1,69 @@
 import React, { useEffect, useState, useRef } from 'react';
-import {useAuth} from '../firebase';
-import {  updateDoc, addDoc, collection, onSnapshot,orderBy,query } from 'firebase/firestore';
-import { db } from "../firebase";
+import { useAuth } from '../firebase';
+import { updateDoc, addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 import moment from 'moment';
 import './commentBox.css';
 
 const Posts = () => {
-    const[posts, setPosts] = useState([])
-    const currentUser = useAuth();
-    const postComment = useRef();
-    const [userData, setUserData] = useState(null);
-    useEffect(()=>{
-        const collectionRef = collection(db,"post")
-        const q = query(collectionRef,orderBy("timestamp","desc"))
-        const unsubscribe = onSnapshot(q, (querySnapshot)=>{
-          setPosts(querySnapshot.docs.map(doc => {
-              console.log(doc.data()); // add this line to log the data object
-              return {
-                  ...doc.data(),
-                  id: doc.id,
-                  timestamp: doc.data().timestamp?.toDate().getTime(),
-                  username: doc.data().userName?.userName
-              }
-          }));
-      });
+  const [posts, setPosts] = useState([]);
+  const currentUser = useAuth();
+  const postComments = useRef([]);
+  const [userData, setUserData] = useState(null);
 
-        return unsubscribe
-    },[])
+  useEffect(() => {
+    const collectionRef = collection(db, 'post');
+    const q = query(collectionRef, orderBy('timestamp', 'desc'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setPosts(
+        querySnapshot.docs.map((doc) => {
+          console.log(doc.data()); // add this line to log the data object
+          return {
+            ...doc.data(),
+            id: doc.id,
+            timestamp: doc.data().timestamp?.toDate().getTime(),
+            username: doc.data().userName?.userName,
+          };
+        })
+      );
+    });
 
-    const uploadComment = async (postId) => {
-      const commentRef = collection(db,'post',postId, "comments")
-      await addDoc(commentRef,{
-        comment : postComment.current.value,
-        username: currentUser.displayName,
-      });
-  }
+    return unsubscribe;
+  }, []);
 
-  
+  const uploadComment = async (postId, postIndex) => {
+    const commentRef = collection(db, 'post', postId, 'comments');
+    await addDoc(commentRef, {
+      comment: postComments.current[postIndex].value,
+      username: currentUser.displayName,
+    });
 
-    return (
-        <div>
-          {posts.map((post) => (
-            <div key={post.id} className='bg-white rounded-lg shadow-xl p-8 w-1/2 m-auto mb-4'>
+    postComments.current[postIndex].value = '';
+  };
 
-              <div className='text-base font-bold'>{post.userName?.userName}</div>
-              <div className=' flex space-x-3'>
-                {post.images.map((image, index) => (
-                  <img key={index}  src={image} width={"200px"} />
-                ))}
-              </div>
-              <div> <span className='text-base p font-bold'>{post.userName?.userName}</span>: {post.post} </div>
+  return (
+    <div>
+      {posts.map((post, index) => (
+        <div key={post.id} className='bg-white rounded-lg shadow-xl p-8 w-1/2 m-auto mb-4'>
+          <div className='text-base font-bold'>{post.userName?.userName}</div>
+          <div className=' flex space-x-3'>
+            {post.images.map((image, index) => (
+              <img key={index} src={image} width={'200px'} />
+            ))}
+          </div>
+          <div>
+            <span className='text-base p font-bold'>{post.userName?.userName}</span>: {post.post}
+          </div>
 
-              <input className="comment-box" ref={postComment} type="text" placeholder='add a comment...'/>
+          <input className='comment-box' ref={(el) => (postComments.current[index] = el)} type='text' placeholder='add a comment...' />
 
-              <button onClick={() => uploadComment(post.id)}>post</button>
+          <button onClick={() => uploadComment(post.id, index)}>post</button>
 
-              <p className='mt-3 text-xs text-right text-gray-400'>{moment(post.timestamp).fromNow()}</p>
-            </div>
-          ))}
+          <p className='mt-3 text-xs text-right text-gray-400'>{moment(post.timestamp).fromNow()}</p>
         </div>
-      )
-              }
- export default Posts;
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
