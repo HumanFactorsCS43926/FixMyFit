a2c0caae6be2221574a6dd138ddcdc92ee644021
r_m672 <r_m672@txstate.du>
2023-04-18 00:04:15 -0500

    changes to finally show the wardrobe

--- FixMyFit-app-main/src/page/dropzoneWar.js
+++ FixMyFit-app-main/src/page/dropzoneWar.js
@@ -1,13 +1,26 @@
 import React, { useCallback ,useState, useRef, useMemo, useEffect} from 'react'
 import { useDropzone } from 'react-dropzone';
 import {db,storage,useAuth} from '../firebase';
-import { onSnapshot, orderBy, query, addDoc, getDoc, getDocs, arrayUnion, updateDoc, doc, collection, serverTimestamp } from 'firebase/firestore';
+import { where, QuerySnapshot, onSnapshot, orderBy, query, addDoc, getDoc, getDocs, arrayUnion, updateDoc, doc, collection, serverTimestamp } from 'firebase/firestore';
 import { ref, getDownloadURL, uploadBytes} from "@firebase/storage";
+import { getAuth } from 'firebase/auth';
 
 
 function Dropzone({clothingType}) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [userPosts, setUserPosts] = useState([]);
+  const [userPants, setUserPants] = useState([]);
+  const [userShirts, setUserShirts] = useState([]);
+  const [userShoes, setUserShoes] = useState([]);
+  const [userShorts, setUserShorts] = useState([]);
+  const [userSocks, setUserSocks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const currentUser = useAuth();
-  const [posts, setPosts] = useState([]);
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const [username, setUsername] = useState(false);
+  const userID = user.uid;
   const [selectedImages, setSelectedImages]= useState([]);
   const [userData, setUserData] = useState(null); // state to store user data
   
@@ -43,6 +56,7 @@ function Dropzone({clothingType}) {
       const userDoc = await getDoc(doc(db, 'users', currentUser.uid));
       if (userDoc.exists()) {
         const userData = userDoc.data();
+        setUsername(userData.userName);
         console.log(userData.userName);
         setUserData(userData);
       } else {
@@ -112,28 +126,68 @@ function Dropzone({clothingType}) {
     getUserData();
   }, [currentUser]);
 
-  useEffect(() => {
-    if (!userData) {
-      console.log("User data is not available yet");
-      return;
-    }
-    const collectionRef = collection(db, 'users', currentUser.uid, clothingType);
-    const q = query(collectionRef, orderBy('timestamp', 'desc'));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      setPosts(
-        querySnapshot.docs.map((doc) => {
-          console.log(doc.data()); // add this line to log the data object
-          return {
-            ...doc.data(),
-            id: doc.id,
-            timestamp: doc.data().timestamp?.toDate().getTime()
-          };
-        })
-      );
-    });
 
-    return unsubscribe();
-  }, []);
+  useEffect(() => {
+    if(userID) {
+      const pantsRef = collection(db, 'users', userID, 'pants');
+      const shirtsRef = collection(db, 'users', userID, 'shirts');
+      const shoesRef = collection(db, 'users', userID, 'shoes');
+      const shortsRef = collection(db, 'users', userID, 'shorts');
+      const socksRef = collection(db, 'users', userID, 'socks');
+      //need to figure out orderBy() after seeing a wardrobe collection************************
+      const qPants = query(pantsRef);
+      const updatePants = onSnapshot(qPants, (querySnapshot) => {
+        setUserPants(
+          querySnapshot.docs.map((doc) => {
+            return {
+              ...doc.data()
+            };
+          })
+        );
+      });
+      const qShirts = query(shirtsRef);
+      const updateShirts = onSnapshot(qShirts, (querySnapshot) => {
+        setUserShirts(
+          querySnapshot.docs.map((doc) => {
+            console.log(doc.data());
+            return {
+              ...doc.data()
+            };
+          })
+        );
+      });
+      const qShoes = query(shoesRef);
+      const updateShoes = onSnapshot(qShoes, (querySnapshot) => {
+        setUserShoes(
+          querySnapshot.docs.map((doc) => {
+            return {
+              ...doc.data()
+            };
+          })
+        );
+      });
+      const qShorts = query(shortsRef);
+      const updateShorts = onSnapshot(qShorts, (querySnapshot) => {
+        setUserShorts(
+          querySnapshot.docs.map((doc) => {
+            return {
+              ...doc.data()
+            };
+          })
+        );
+      });
+      const qSocks = query(socksRef);
+      const updateSocks = onSnapshot(qSocks, (querySnapshot) => {
+        setUserSocks(
+          querySnapshot.docs.map((doc) => {
+            return {
+              ...doc.data()
+            };
+          })
+        );
+      });
+    }
+  }, [userID]);
 
   return(
     <div>
@@ -146,15 +200,45 @@ function Dropzone({clothingType}) {
         group relative flex w-full justify-center rounded-md border border-transparent 
         bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">upload
       </button>
+      
       <div>
-      {posts.map((post) => (
-        <div key={post.id}>
-          {post.images.map((image, index) => (
-            <img key={index} src={image} width={'auto'} />
-          ))}
-        </div>
-      ))}
-    </div>
+        {clothingType === 'shirts' && userShirts.map((shirts, index) => (
+          <div key={shirts.id} className='bg-white rounded-lg shadow-xl p-8 mb-4'>
+            {shirts.images.map((image, index) => (
+              <img key={index} src={image} width={'auto'} />
+            ))}
+          </div>
+        ))}
+        {clothingType === 'pants' && userPants.map((pants, index) => (
+          <div key={pants.id} className='bg-white rounded-lg shadow-xl p-8 mb-4'>
+            {pants.images.map((image, index) => (
+              <img key={index} src={image} width={'auto'} />
+            ))}
+          </div>
+        ))}
+        {clothingType === 'shorts' && userShorts.map((shorts, index) => (
+          <div key={shorts.id} className='bg-white rounded-lg shadow-xl p-8 mb-4'>
+            {shorts.images.map((image, index) => (
+              <img key={index} src={image} width={'auto'} />
+            ))}
+          </div>
+        ))}
+        {clothingType === 'socks' && userSocks.map((socks, index) => (
+          <div key={socks.id} className='bg-white rounded-lg shadow-xl p-8 mb-4'>
+            {socks.images.map((image, index) => (
+              <img key={index} src={image} width={'auto'} />
+            ))}
+          </div>
+        ))}
+        {clothingType === 'shoes' && userShoes.map((shoes, index) => (
+          <div key={shoes.id} className='bg-white rounded-lg shadow-xl p-8 mb-4'>
+            {shoes.images.map((image, index) => (
+              <img key={index} src={image} width={'auto'} />
+            ))}
+          </div>
+        ))}
+      </div>
+      
   </div>
 );
           }
